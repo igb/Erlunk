7a500fd8b3de4c88273cfa1c9ac022296333643f
Ian Brown <igb@hccp.org>
2013-01-19 15:46:28 -0800

    pagination and download working

--- src/erlunk.erl
+++ src/erlunk.erl
@@ -1,6 +1,6 @@
 -module(erlunk).
 
--export([login/3,extract_token/1, search/3, get_job_status/3,get_results/3]).
+-export([login/3,extract_token/1, search/3, get_job_status/3,get_results/3, download_source/5]).
 
 
 
@@ -30,18 +30,112 @@ get_results_when_ready(Sid, Endpoint, Token)->
 	     get_results_when_ready(Sid, Endpoint, Token)
     end.
 
+
+download_source(Index, Source, LocalFile, Endpoint, Token)->
+    Query=lists:flatten(["index=", Index, " source=\"", Source, "\""]),
+    Results=search(Query, Endpoint, Token),
+    FirstToLast=lists:reverse(Results),
+
+    
+    
+
+    {ok, FileDescriptor} = file:open(LocalFile, [write]),
+    PrintFun=fun(X)-> {{offset,_},Fields}=X,{_,Raw}=lists:keyfind("_raw", 1, Fields),
+		      RawLines=re:replace(Raw, "\\\n", "\n", [global, {return, list}]),
+		      io:format(FileDescriptor, "~s~n", [RawLines])
+	     end,
+    lists:map(PrintFun,FirstToLast),
+    file:close(FileDescriptor).
+    
+
+
+%% gets ALL the results
 get_results(Sid, Endpoint, Token)->
-     {ok, {{_, StatusCode, _}, ResponseHeaders, Body}}=httpc:request(get, {lists:flatten([Endpoint, "/services/search/jobs/", Sid, "/results?output_mode=xml"]), [{"Authorization", lists:flatten(["Splunk ", Token])}]}, [],[]),
+    get_results(Sid, Endpoint, Token, 0, []).
+
+
+get_results(Sid, Endpoint, Token, Offset, Acc)->
+     {ok, {{_, StatusCode, _}, ResponseHeaders, Body}}=httpc:request(get, {lists:flatten([Endpoint, "/services/search/jobs/", Sid, "/results?output_mode=xml&offset=", integer_to_list(Offset)]), [{"Authorization", lists:flatten(["Splunk ", Token])}]}, [],[]),
     case StatusCode of
 	200 ->
 	    Body,
+	    % write to tmp file for debugging...
 	    {ok, FileDescriptor} = file:open("/tmp/output.txt", [write]),
 	    io:format(FileDescriptor, "~s~n~nend~n~n", [Body]),
-	    file:close(FileDescriptor);
+	    file:close(FileDescriptor),
+	    
+	    % parse XML
+	    Results=parse_results(Body),
+	    case Results of
+		no_results->Acc;
+		_-> NewAcc=lists:append([Acc,Results]),
+		    [{{offset, NewOffsetStr}, _}|_]=lists:nthtail(length(Results) -1, Results),
+		    io:fwrite("new offset: ~p~n",[NewOffsetStr]),
+		    {NewOffsetInt,[]}=string:to_integer(NewOffsetStr),
+		    get_results(Sid, Endpoint, Token, NewOffsetInt + 1, NewAcc)
+	    end;
+
+	
 	_  -> {err, StatusCode}
     end.
     
 
+parse_results(Body)->
+    {Xml, _}=xmerl_scan:string(Body, [{space, 'normalize'}]),
+    SimplifiedXml=xmerl_lib:simplify_element(Xml),
+    {results,_,Results}=SimplifiedXml,
+    case Results of
+	[]->no_results;
+	_->
+	    [" ", Metadata|ResultData]=Results,
+	    Fields=parse_metadata(Metadata),
+	    CleanedResultData=strip_whitespace_from_xml(ResultData),
+%	    io:fwrite("~p~n", [CleanedResultData]),
+	    lists:map(fun(X)->
+			      {result,[{offset,Offset}],ResultFields}=X,
+			      {{offset, Offset},
+			       lists:map(fun(Y)-> io:fwrite("~p~n",[Y]),
+						 {field,[{k,FieldName}], ValueStruct}=Y,
+						 Value=extract_value(ValueStruct),
+						 {FieldName, Value}
+					 end,
+					 strip_whitespace_from_xml(ResultFields)) } 
+		      end,
+		      CleanedResultData)
+    end.
+
+
+
+flatten_raw_content(RawContent)->     
+    flatten_raw_content(RawContent, []).
+
+flatten_raw_content([H|T], Acc)->
+    case H of
+	{sg,[_],[Content]}->NewAcc=lists:flatten([Acc, Content]),
+			    flatten_raw_content(T, NewAcc);
+	Raw -> NewAcc=lists:flatten([Acc,Raw]),
+	       flatten_raw_content(T, NewAcc)
+	end; 
+flatten_raw_content([], Acc)->
+    Acc.
+
+
+extract_value(ValueStruct)->
+    case ValueStruct of
+	[" ",{value,[],[{text,[],[Value]}]}," "]->Value;
+	[" ",{value,[{h,"1"}],[{text,[],[Value]}]}," "]->Value;
+	[" ",{value,[],[{text,[],[ValueOne]}]}," ",{value,[],[{text,[],[ValueTwo]}]}," "]->lists:flatten([ValueOne, " ", ValueTwo]);
+	[{v,[{'xml:space',"preserve"},{trunc,"0"}], RawValue}]-> flatten_raw_content(RawValue)
+    end.
+
+        
+parse_metadata(Metadata)->    
+    {meta,[],[_,{fieldOrder,[],Fields},_]}=Metadata,
+    FilteredFields=strip_whitespace_from_xml(Fields),
+    [X || {field,[],[X]} <- FilteredFields].
+
+strip_whitespace_from_xml(XmlStruct)->
+    FilteredFields=lists:filter(fun(X)-> X/=" " end, XmlStruct).
 
 get_job_status(Sid, Endpoint, Token)->
     Url=lists:flatten([Endpoint, "/services/search/jobs/", Sid]),
@@ -52,7 +146,6 @@ get_job_status(Sid, Endpoint, Token)->
 	200 -> {Xml, _}=xmerl_scan:string(Body, [{space, 'normalize'}]),            
 	       SimplifiedXml=xmerl_lib:simplify_element(Xml),
 	       {entry,_, EntryElements}=SimplifiedXml,
-	    %   FilteredEntryElements=lists:filter(fun(X)-> X/=" " end, EntryElements), 
 	       {content,[{type,"text/xml"}],
 		[" ",
 		 {'s:dict',[], ContentDictionaryEntries}|_]}=lists:keyfind(content, 1, EntryElements),
